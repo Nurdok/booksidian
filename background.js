787292858a514481ab32c884343d68aa4e4530c3
Amir Rachum <amir@rachum.com>
2021-09-17 10:42:20 +0300

    Create book details table plus some nice refactoring.

--- background.js
+++ background.js
@@ -1,16 +1,62 @@
-function GetObsidianUri() {
-    let book_title = document.getElementById("bookTitle").innerText.trim();
-    let authors = new Array();
+//const nl = "%0A";  // #
+//const hash = "%23";  // \n
+
+function dedent(text) {
+    let re_whitespace = /^([ \t]*)(.*)\n/gm;
+    let l, m, i;
+    while ((m = re_whitespace.exec(text)) !== null) {
+        if (!m[2]) continue;
+        if (l = m[1].length) {
+            i = (i !== undefined) ? Math.min(i, l) : l;
+        } else break;
+    }
+    if (i)
+        text = text.replace(new RegExp('^[ \t]{' + i + '}(.*\n)', 'gm'), '$1');
+    return text;
+}
+
+function getBookFromGoodreads() {
+    let book = new Object();
+    book.full_title = document.getElementById("bookTitle").innerText.trim();
+    let split_title = book.full_title.split(":", 2);
+    book.short_title = split_title[0];
+    book.title_tag = split_title[1];
+    book.authors = new Array();
     document.querySelectorAll(".authorName span").forEach(function(elm) {
-        authors.push(elm.innerText);
+        book.authors.push(elm.innerText);
     });
-    let formatted_authors = authors.map(function(author) {
+    document.querySelectorAll("#details .row").forEach(function(node) {
+    if (node.innerText.includes("Published")) {
+        book.publication_year = node.innerText.match(/\d{4}/)[0];
+    }
+});
+
+    return book;
+}
+
+function getObsidianUri(book) {
+    let formatted_authors = book.authors.map(function(author) {
         return `[[${author}]]`;
-    }).join(", ")
-    let nl = "%0A";
-    let hash = "%23";
-    let content = `---${nl}tags:${nl}- book${nl}${nl}---${nl}${hash} ${book_title}${nl}By ${formatted_authors}`;
-    let obsidian_uri = `obsidian://new?vault=notes&name=${book_title}&content=${content}`;
+    }).join(", ");
+
+    let content = dedent(`
+        ---
+        tags:
+        - book
+        
+        ---
+        # ${book.short_title}
+        
+        | | |
+        | - | - |
+        | **Full title** | ${book.full_title} |
+        | **Authors** | ${formatted_authors} |
+        | **Publication Year** | ${book.publication_year} |
+        | | |
+    `);
+    content = content.replaceAll('\n', '%0A');
+    content = content.replaceAll('#', '%23');
+    let obsidian_uri = `obsidian://new?vault=notes&name=${book.short_title}&content=${content}`;
     console.log(obsidian_uri);
     return obsidian_uri;
 }
@@ -19,10 +65,11 @@ function handleBrowserButtonClick(tab) {
     chrome.scripting.executeScript(
         {
             target: {tabId: tab.id},
-            func: GetObsidianUri,
+            func: getBookFromGoodreads,
         },
         (results) => {
-            var uri = results[0].result;
+            const book = results[0].result;
+            const uri = getObsidianUri(book);
             chrome.tabs.create({url: uri});
         });
 }
