2334220036e29e101b704bcccffb53ff03e076ba
Amir Rachum <amir@rachum.com>
2021-10-20 12:44:47 +0300

    Added `date` and `time` template parameters (#16)
    * Add basic support for `{{ date }}` and `{{ time }}` with formatting.
    
    * Added docs.
    
    * Copied tempalte changes to `background.js`.
    
    * Fixed some HTML.

--- options.js
+++ options.js
@@ -15,9 +15,20 @@ function dedent(text) {
 // Copied from background.js. Should be in a separate module once we figure
 // out how to load modules in background.js...
 function populateTemplate(template, book) {
-    return template.replaceAll(/\{\{\s*(\w+)\s*\}\}/g, function(match, property, offset, string) {
+    return template.replaceAll(/\{\{\s*(\w+):?([^}{\n]+)\s*\}\}/g, function(match, property, format, offset, string) {
+        format = format.trim();
         if (book.hasOwnProperty(property)) {
             return book[property];
+        } else if (property === "date") {
+            if (!format) {
+                format = "YYYY-MM-DD";
+            }
+            return moment().format(format);
+        } else if (property === "time") {
+            if (!format) {
+                format = "HH:mm";
+            }
+            return moment().format(format);
         } else {
             return property;
         }
