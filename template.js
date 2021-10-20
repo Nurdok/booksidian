562af8948f3ed23519e5a6ce2736cdf1ec7cdeef
Amir Rachum <amir@rachum.com>
2021-10-20 16:17:48 +0300

    Added luxon

--- template.js
+++ template.js
@@ -1,3 +1,5 @@
+import {DateTime} from "./third_party/luxon.min";
+
 export function populateTemplate(template, book) {
     return template.replaceAll(/\{\{\s*(\w+)\s*\}\}/g, function(match, property, offset, string) {
         if (book.hasOwnProperty(property)) {
