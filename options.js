5befddf4ac9e118d6fbc17f0dbbb153df371f7e5
Amir Rachum <amir@rachum.com>
2021-10-20 15:19:03 +0300

    Revert "Added `date` and `time` template parameters (#16)"
    This reverts commit 2334220036e29e101b704bcccffb53ff03e076ba.

--- options.js
+++ options.js
@@ -15,20 +15,9 @@ function dedent(text) {
 // Copied from background.js. Should be in a separate module once we figure
 // out how to load modules in background.js...
 function populateTemplate(template, book) {
-    return template.replaceAll(/\{\{\s*(\w+):?([^}{\n]+)\s*\}\}/g, function(match, property, format, offset, string) {
-        format = format.trim();
+    return template.replaceAll(/\{\{\s*(\w+)\s*\}\}/g, function(match, property, offset, string) {
         if (book.hasOwnProperty(property)) {
             return book[property];
-        } else if (property === "date") {
-            if (!format) {
-                format = "YYYY-MM-DD";
-            }
-            return moment().format(format);
-        } else if (property === "time") {
-            if (!format) {
-                format = "HH:mm";
-            }
-            return moment().format(format);
         } else {
             return property;
         }
