2e4cf8534302bcd3c803162b9a9d7a608b71d373
Amir Rachum <amir@rachum.com>
2021-10-20 12:28:41 +0300

    Copied tempalte changes to `background.js`.

--- background.js
+++ background.js
@@ -1,13 +1,24 @@
-
 function populateTemplate(template, book) {
-    return template.replaceAll(/\{\{\s*(\w+)\s*\}\}/g, function(match, property, offset, string) {
+    return template.replaceAll(/\{\{\s*(\w+):?([^}{\n]+)\s*\}\}/g, function(match, property, format, offset, string) {
+        format = format.trim();
         if (book.hasOwnProperty(property)) {
             return book[property];
+        } else if (property === "date") {
+            if (!format) {
+                format = "YYYY-MM-DD";
+            }
+            return moment().format(format);
+        } else if (property === "time") {
+            if (!format) {
+                format = "HH:mm";
+            }
+            return moment().format(format);
         } else {
             return property;
         }
     });
 }
+
 
 function getBookFromGoodreads() {
     let book = new Object();
