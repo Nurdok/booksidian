2e87ec46597f4d228874262557afaac602176e4a
Amir Rachum <amir@rachum.com>
2021-10-20 15:23:20 +0300

    Move `populateTemplate` to a separate module.

--- template.js
+++ template.js
@@ -1,24 +1,10 @@
-import "./third_party/moment.js";
-
 export function populateTemplate(template, book) {
-    return template.replaceAll(/\{\{\s*(\w+):?([^}{\n]+)\s*\}\}/g, function(match, property, format, offset, string) {
-        format = format.trim();
+    return template.replaceAll(/\{\{\s*(\w+)\s*\}\}/g, function(match, property, offset, string) {
         if (book.hasOwnProperty(property)) {
             return book[property];
-        } else if (property === "date") {
-            if (!format) {
-                format = "YYYY-MM-DD";
-            }
-            return moment().format(format);
-        } else if (property === "time") {
-            if (!format) {
-                format = "HH:mm";
-            }
-            return moment().format(format);
         } else {
             return property;
         }
     });
 }
-
 
