ea3b6cdcd2a8accaa57c5f40bae5c2d6e747ea1f
Amir Rachum <amir@rachum.com>
2021-10-22 08:49:57 +0300

    Added `date` and `time` template parameters (now using Luxon) (#19)
    * Added luxon
    
    * Added `date` and `time` template parameters

--- template.js
+++ template.js
@@ -1,7 +1,20 @@
+import {DateTime} from "./third_party/luxon.min.js";
+
 export function populateTemplate(template, book) {
-    return template.replaceAll(/\{\{\s*(\w+)\s*\}\}/g, function(match, property, offset, string) {
+    return template.replaceAll(/\{\{\s*(\w+):?([^}{\n]+)\s*\}\}/g, function(match, property, format, offset, string) {
+        format = format.trim();
         if (book.hasOwnProperty(property)) {
             return book[property];
+        } else if (property === "date") {
+            if (!format) {
+                format = "yyyy-MM-dd";
+            }
+            return DateTime.now().toFormat(format);
+        } else if (property === "time") {
+            if (!format) {
+                format = "HH:mm";
+            }
+            return DateTime.now().toFormat(format);
         } else {
             return property;
         }
