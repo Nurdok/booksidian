9e3ef2a18bb4589aa2cabff7a08fc3018fdce950
Amir Rachum <amir@rachum.com>
2022-10-10 14:19:26 +0300

    Fix merge

--- goodreads/handler.js
+++ goodreads/handler.js
@@ -109,10 +109,10 @@ function getBookFromGoodreads() {
 
 async function readOptionsFromStorage() {
     return {
-        vault: await getStorageValue({vault: DEFAULT_OPTIONS.vault})[0],
-        file_location:  await getStorageValue({file_location: DEFAULT_OPTIONS.file_location})[0],
-        note_title: await getStorageValue({note_title: DEFAULT_OPTIONS.note_title})[0],
-        note_content: await getStorageValue({note_content: DEFAULT_OPTIONS.note_content})[0],
+        vault: (await getStorageValue({vault: DEFAULT_OPTIONS.vault})).vault,
+        file_location:  (await getStorageValue({file_location: DEFAULT_OPTIONS.file_location})).file_location,
+        note_title: (await getStorageValue({note_title: DEFAULT_OPTIONS.note_title})).note_title,
+        note_content: (await getStorageValue({note_content: DEFAULT_OPTIONS.note_content})).note_content,
     }
 }
 
